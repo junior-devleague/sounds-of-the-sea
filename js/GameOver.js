06b767133224e166ebadc45771e10aa7ef2b2686
sgalutira <sheena.galutira@gmail.com>
2017-01-22 12:35:26 -1000

    added human speech text on end game

--- js/GameOver.js
+++ js/GameOver.js
@@ -4,4 +4,18 @@ function gameOver(){
   var endText = "GAME OVER! \n Enjoy your marine park entertainment.";
   var style = { font: "32px Arial Black", fill: "#ffffff", align: "center"};
   var text = game.add.text(50, 160, endText, style);
+  humanTalk();
 };
+
+function humanTalk(){
+  var talkBubble = game.add.graphics(400, 0);
+  talkBubble.beginFill(0xffffff);
+  talkBubble.lineStyle(2, 0x000000, 1);
+  talkBubble.drawEllipse(100, 40, 80, 50);
+  talkBubble.endFill();
+
+  var humanTextBox = game.add.graphics(400, 100);
+  var humanText = "You're going to a \n marine life park!!!";
+  var style = { font: "14px Arial", fill: "#000000", align: "center"};
+  var text = game.add.text(445, 20, humanText, style);
+}
