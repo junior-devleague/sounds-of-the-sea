6b21d0a46abffb21f0b58cdaa87459fa1b832872
sgalutira <sheena.galutira@gmail.com>
2017-01-22 01:36:01 -1000

    changed waves position

--- js/Wave.js
+++ js/Wave.js
@@ -15,11 +15,11 @@ function waveLevelHigh(){
 
 function drawSin(){
   // // Draw sinData
-  var gfx = game.add.graphics(366, 500);
+  var gfx = game.add.graphics(387, 500);
   gfx.lineStyle(3, 0x24efdc);
   for (var i = 0; i < sin.length; i ++) {
     // gfx.lineTo(368 + i, 500 + sin[i]);
-    gfx.lineTo(22 + i, sin[i]);
+    gfx.lineTo(22 + i, 40 + sin[i]);
 
   }
 };
@@ -29,7 +29,16 @@ function waveBox(){
   var waveBox = game.add.graphics(100, 100);
   waveBox.beginFill(0xf3faff);
   waveBox.lineStyle(2, 0x000000, 1);
-  waveBox.drawRect(265, 350, 405, 100);
+  waveBox.drawRect(285, 390, 405, 100);
   waveBox.endFill();
+}
+
+//Create profile box
+function profileBox(){
+  var profileBox = game.add.graphics(100, 100);
+  profileBox.beginFill(0xf3faff);
+  profileBox.lineStyle(2, 0x000000, 1);
+  profileBox.drawRect(165,390, 100, 100);
+  profileBox.endFill();
 }
 
